7157846b36ddafed51b3967230d81b10249f3089
Behrang <behvandi.behrang@gmail.com>
2020-08-29 12:08:59 +0300

    Added message mapping for websockets

--- server/spring-websockets/src/main/java/com/app/controller/AccountController.java
+++ server/spring-websockets/src/main/java/com/app/controller/AccountController.java
@@ -55,9 +55,9 @@ public class AccountController {
                                     @Valid @RequestBody SimpleBalanceRequestDto simpleBalanceRequestDto,
                                     Principal principal) {
         log.info("Topping account  with number {}, amount {}", number, simpleBalanceRequestDto.getAmount());
-        AccountResponseDto response =  accountFacade.topUp(number, simpleBalanceRequestDto.getAmount());
+        AccountResponseDto response = accountFacade.topUp(number, simpleBalanceRequestDto.getAmount());
         template.convertAndSend("/notifications/data",
-                new NotificationResponseDto(principal.getName(), null,simpleBalanceRequestDto.getAmount(), OperationType.topUp));
+                new NotificationResponseDto(principal.getName(), null, simpleBalanceRequestDto.getAmount(), OperationType.topUp));
         return response;
     }
 
@@ -65,12 +65,45 @@ public class AccountController {
     public AccountResponseDto topUp(@Valid @RequestBody TransferRequestDto transferRequestDto,
                                     Principal principal) {
         log.info("Transferring from  account with number {}, amount {}, " +
-                "to account with number {}", transferRequestDto.getSender(),
+                        "to account with number {}", transferRequestDto.getSender(),
                 transferRequestDto.getAmount(), transferRequestDto.getReceiver());
-        AccountResponseDto accountResponse =  accountFacade.transfer(transferRequestDto.getSender(),
+        AccountResponseDto accountResponse = accountFacade.transfer(transferRequestDto.getSender(),
                 transferRequestDto.getReceiver(), transferRequestDto.getAmount());
         template.convertAndSend("/notifications/data",
-                new NotificationResponseDto(principal.getName(), transferRequestDto.getReceiver(),transferRequestDto.getAmount(), OperationType.topUp));
+                new NotificationResponseDto(principal.getName(), transferRequestDto.getReceiver(), transferRequestDto.getAmount(), OperationType.topUp));
         return accountResponse;
     }
+    @MessageMapping("/withdraw")
+    @SendTo("/response/data")
+    public NotificationResponseDto withdraw(TransferRequestDto transferDtoRequest) {
+        log.info("Withdrawing from account with number {} amount {}", transferDtoRequest.getSender(), transferDtoRequest.getAmount());
+        return NotificationResponseDto.builder()
+                .operation(OperationType.withdraw)
+                .sender(transferDtoRequest.getSender())
+                .amount(transferDtoRequest.getAmount())
+                .build();
+    }
+    @MessageMapping("/top-up")
+    @SendTo("/response/data")
+    public NotificationResponseDto topUp(TransferRequestDto transferDtoRequest) {
+        log.info("Topping up account with number {} amount {}", transferDtoRequest.getSender(), transferDtoRequest.getAmount());
+        return NotificationResponseDto.builder()
+                .operation(OperationType.topUp)
+                .sender(transferDtoRequest.getSender())
+                .amount(transferDtoRequest.getAmount())
+                .build();
+    }
+    @MessageMapping("/transfer")
+    @SendTo("/response/data")
+    public NotificationResponseDto transferNotification(TransferRequestDto transferDtoRequest) {
+        log.info("Transfer {} sum from {} to {}", transferDtoRequest.getAmount(), transferDtoRequest.getSender(), transferDtoRequest.getReceiver());
+        return NotificationResponseDto.builder()
+                .operation(OperationType.transfer)
+                .sender(transferDtoRequest.getSender())
+                .receiver(transferDtoRequest.getReceiver())
+                .amount(transferDtoRequest.getAmount())
+                .build();
+    }
+
+
 }
